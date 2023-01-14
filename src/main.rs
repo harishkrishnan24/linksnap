86207ef54d2e914e3b5dd4d04f42ec6c439c15a9
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-01-14 13:15:32 +0530

    API using actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::env;
+use actix_web::{App, http, server};
+use actix_web::middleware::Logger;
+use log::info;
+use crate::state::State;
+use crate::route_handlers::{index, links, add_link, rm_link};
+
+mod links;
+mod route_handlers;
+mod state;
+
+fn init_env() {
+    env::set_var("RUST_LOG", "linksnap=info");
+    env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+    info!("Starting http server: 127.0.0.1:8080");
+}
+
 fn main() {
-    println!("Hello, world!");
+    init_env();
+    let system = actix::System::new("linksnap");
+    let state = State::init();
+
+    let web_app = move || {
+        App::with_state(state.clone())
+            .middleware(Logger::default())
+            .route("/", http::Method::GET, index)
+            .route("/links", http::Method::GET, links)
+            .route("/add", http::Method::POST, add_link)
+            .route("/rm", http::Method::DELETE, rm_link)
+    };
+
+    server::new(web_app).bind("127.0.0.1:8080").unwrap().start();
+    let _ = system.run();
 }
